b12c0e2ced34f47dcc4bc8a5da6190511199e01a
fellipeemvocacao <fellipebarros972@gmail.com>
2026-04-01 17:11:21 -0300

    Upgrade login_seguro.py

--- login_seguro.py
+++ login_seguro.py
@@ -1,49 +1,74 @@
+#Login
+nome_usuario = input('Digite seu nome de usuário: ')
+senha_usuario = input('Digite sua senha: ')
+
+
+print('Login realizado com sucesso')
+
+
+
+print('\nTela de login - Meet\n')
+
+nome_usuario = input('Digite seu login para continuar: ')
+senha_usuario = input('Digite sua senha para continuar: ')
+
+def realizar_Login():
+
+   senha_padrao:1234 
+   print('\nTela de login - Meet\n')
+
+   nome_usuario = input('Digite seu login para continuar: ')
+   senha_usuario = input('Digite sua senha para continuar: ')
+   print(f' Bem vindo {nome_usuario}!')
+
+
+ if senha_padrao: 
+    print('Senha correta!')
+   
+   else:
+    print('Senha incorreta')
+    print(f' Bem vindo, {nome_usuario}!')
+
+    print('---------------------------\n')
+
+
+realizar_Login()
+
+
 #Login seguro
 
-import getpass
-import hashlib
-
-def gerar_hash(senha):
-    """Transforma a senha em um hash SHA-256."""
-    return hashlib.sha256(senha.encode()).hexdigest()
-
 def realizar_login_seguro():
-    
-    hash_senha_cadastrada = "Ivanlindo"
-    
-    print('\n--- Sistema de Login de Alta Segurança ---')
+    senha_padrao = "1234"
+
+    print('\n--- Sistema de Login Seguro ---')
     nome_usuario = input('Digite seu login: ')
-    
-    
-    senha_digitada = getpass.getpass('Digite sua senha: ')
-    
+    senha_digitada = input('Digite sua senha: ')
 
-    hash_tentativa = gerar_hash(senha_digitada)
-
-    if hash_tentativa == hash_senha_cadastrada:
-        print(f'\n[SUCESSO] Acesso autorizado. Olá, {nome_usuario}!')
+    if senha_digitada == senha_padrao:
+        print(f'\n[SUCESSO] Bem-vindo, {nome_usuario}! Acesso concedido.')
     else:
-        print('\n[ERRO] Credenciais inválidas!')
+        print('\n[ERRO] Senha incorreta! Acesso negado.')
 
-    print('------------------------------------------\n')
+        print('---------------------------\n')
 
-if __name__ == "_main_":
-    realizar_login_seguro()
+realizar_login_seguro()
+
 
 
 #Login Invisível
 
-import getpass  
+
+import getpass
+
 
 def realizar_login_invisivel():
-    
+
     senha_padrao = "1234"
 
     print('\n--- Sistema de Login Protegido ---')
     nome_usuario = input('Digite seu login: ')
 
-
-    senha_digitada = getpass.getpass('Digite sua senha (os caracteres não aparecerão): ')
+    senha_digitada = getpass.getpass('Digite sua senha (os caracteres não aparecerão):')
 
     if senha_digitada == senha_padrao:
         print(f'\n[SUCESSO] Bem-vindo, {nome_usuario}!')
@@ -52,8 +77,7 @@ def realizar_login_invisivel():
 
     print('----------------------------------\n')
 
-if __name__ == "__main__":
-    realizar_login_invisivel()
+realizar_login_invisivel
 
 
 #Login com tentativas
